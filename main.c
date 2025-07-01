7b8b25ae8a129877cb7cf1ae87eaa6cc50dfe762
MauroGutMB <mgmbarros2006@gmail.com>
2025-07-01 19:43:09 -0300

    Esqueleto do trabalho final

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "src/funcs.h"
+
+int main(){
+
+  printf("This is a C standard aplication prrototype.\n");
+  HelloWorld();
+
+  return 0;
+}
---
+++ src/funcs.c
@@ -0,0 +1,9 @@
+#include "funcs.h"
+#include <stdio.h>
+
+void HelloWorld(){
+
+  printf("This is a C standard module.\n");
+  printf("Hello, World!\n");
+
+}
