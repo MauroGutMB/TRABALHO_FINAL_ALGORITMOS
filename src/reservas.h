1ccc94467469a77551e2e323738e28c6574e78c5
BRENO GUEDES <brenoguedes186@gmail.com>
2025-07-02 22:31:27 -0300

    Update reservas.h

--- src/reservas.h
+++ src/reservas.h
@@ -1,1 +1,31 @@
+#ifndef RESERVAS_H
+#define RESERVAS_H
 
+#define MAX_VOOS 100
+
+// Struct para o cadastro
+typedef struct {
+    int numero;
+    char origem[50];
+    char destino[50];
+    char data[11];         
+    char hora_partida[6];   
+    char hora_chegada[6];   
+} Voo;
+
+// Struct para pesquisa
+typedef struct {
+    char origem[50];
+    char destino[50];
+    char data[11];          
+} Pesquisa;
+
+extern Voo voos[MAX_VOOS];
+extern int qtd_voos;
+
+// Funções
+void cadastrarVoo();     
+void exibirVoos();     
+void pesquisarVoos();  
+
+#endif
